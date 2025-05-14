92a9d25db6966feff0c84b9ea233f00ebed9a385
Enrico <168141917+Enrico-Alvarenga@users.noreply.github.com>
2025-05-14 14:21:32 -0300

    Update main.java

--- exercicios/Etapa 2/main.java
+++ exercicios/Etapa 2/main.java
@@ -1,1 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        SettingsManager config = SettingsManager.getInstance();
+        config.setPreferredTransport("bike");
+
+        TransportFactory factory = new TransportFactory();
+        TransportMode transport = factory.create("bike");
+
+        MapService mapService = new ExternalMapAdapter(new LegacyMapProvider());
+        RouteService routeService = new CachedRouteProxy(new BaseRouteService(mapService));
+
+        RoutePlanner planner = new RoutePlanner();
+        planner.setStrategy(new EcoFriendlyStrategy());
+
+        AppFacade facade = new AppFacade(planner, routeService, config);
+        facade.showRouteWithEnhancements("Centro", "Parque Ibirapuera", "bike");
+    }
+}
 
